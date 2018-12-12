85901fb15ca926f6df6dd087abd1c99f3c01c904
Kevin Hibma <khibma@gmail.com>
2018-12-11 22:26:13 -0500

    tracking war day

--- app.py
+++ app.py
@@ -24,13 +24,16 @@ t = tokenAzure if platform != "win32" else tokenLocal
 CR = clash.clash(t, clanID)
 print(CR)
 
+def fixTime(t):   
+    return (t - datetime.timedelta(hours=5)).strftime("%Y-%m-%d %H:%M")
+
 @app.route('/')
 def index(everything=CR):
 
    if (datetime.datetime.now() - CR.updateTime).seconds > 120:
         CR.clanInfo = CR.getClanInfo()        
         CR.updateTime = datetime.datetime.now()
-   return render_template('index.html', everything=CR.clanInfo, t=CR.token, updatetime=CR.updateTime.strftime("%Y-%m-%d %H:%M"))
+   return render_template('index.html', everything=CR.clanInfo, t=CR.token, updatetime=fixTime(CR.updateTime))
 
 @app.route('/user/', methods=['GET', 'POST'])
 @app.route('/user/<u>', methods=['GET'])
@@ -38,6 +41,7 @@ def userRender(u=None):
    
    playerInfo = 9999
    uchest = {'items':[]}
+   warInfo = {'warWins': 0, 'warLose': 0, 'collectWin': 0, 'collectLose': 0}
 
    if not u:
       form = ReusableForm(request.form)
@@ -51,9 +55,21 @@ def userRender(u=None):
    if u:
       playerInfo = CR.getPlayerInfo(u)
       uchest = CR.getUpcomingChests(u)
-      #battle = CR.getBattleLog(u)
-        
-   return render_template('user.html', pi = playerInfo, chst=uchest['items'], clanName=CR.clanName, updatetime=CR.updateTime.strftime("%Y-%m-%d %H:%M"))
+      battle = CR.getBattleLog(u)
+
+      for b in battle:
+          if b['type'] == "clanWarCollectionDay":
+            if b['team'][0]['crowns'] > b['opponent'][0]['crowns']:
+              warInfo['collectWin'] +=1
+            else:
+              warInfo['collectLose'] +=1
+          if b['type'] == "clanWarWarDay":
+            if b['team'][0]['crowns'] > b['opponent'][0]['crowns']:
+              warInfo['warWins'] +=1
+            else:
+              warInfo['warLose'] +=1
+    
+   return render_template('user.html', pi = playerInfo, w=warInfo, chst=uchest['items'], clanName=CR.clanName, updatetime=fixTime(CR.updateTime))
 
 if __name__ == '__main__':
 
